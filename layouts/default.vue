1aff7fa6ca1e6824b1a0f9fb79545865c9bcf90c
Cássio Almeida <cassio.almeida@jetbov.com>
2024-03-05 17:56:47 -0300

    Estruturação inicial do projeto com utilização do Atomic Design

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+			<div class="pa-4">Comercial Case</div>
+    </v-navigation-drawer>
+    <v-main>
+      <v-container class="py-8 px-6" fluid>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const drawer = ref(null)
+</script>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null,
+    })
+  }
+</script>
